6cf5715af7c46799416cf8f96c8cf7444361ac01
Ian Naylan <42600121+i1d9@users.noreply.github.com>
2024-08-28 11:21:32 +0300

    library setup

---
+++ lib/metaex.ex
@@ -0,0 +1,18 @@
+defmodule Metaex do
+  @moduledoc """
+  Documentation for `Metaex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Metaex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/metaex_test.exs
@@ -0,0 +1,8 @@
+defmodule MetaexTest do
+  use ExUnit.Case
+  doctest Metaex
+
+  test "greets the world" do
+    assert Metaex.hello() == :world
+  end
+end
